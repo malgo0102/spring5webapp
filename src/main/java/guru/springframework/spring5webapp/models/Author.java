0d8f9bac86495213102a701fc0ab517e88ecee03
Malgo0102 <mal.wit@hotmail.com>
2019-12-29 19:43:39 +0100

    Added Spring Data JPA Repositories

--- src/main/java/guru/springframework/spring5webapp/models/Author.java
+++ src/main/java/guru/springframework/spring5webapp/models/Author.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp.model;
+package guru.springframework.spring5webapp.models;
 
 import javax.persistence.*;
 import java.util.HashSet;
---
+++ src/main/java/guru/springframework/spring5webapp/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring5webapp.repository;
+
+import guru.springframework.spring5webapp.models.Author;
+import guru.springframework.spring5webapp.models.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
